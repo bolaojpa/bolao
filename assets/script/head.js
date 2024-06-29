e42de9f4d8c5b6ba7f5d86900077d5c36ba12448
Bolao JPA <bolaojpa@gmail.com>
2024-06-29 11:10:14 -0300

    Tentando colocar metatags miniaturas do site em redes sociais

--- assets/script/head.js
+++ assets/script/head.js
@@ -4,20 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Bolão Euro 2024</title>
 
-<meta name="title" content="Bolão Euro 2024" />
-<meta name="description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
-
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://bolaojpa.github.io/bolaoeuro2024/" />
-<meta property="og:title" content="Bolão Euro 2024" />
-<meta property="og:description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
-<meta property="og:image" content="https://i.ibb.co/FDkmQPS/Banner-Euro-2024-Azul-small.png" />
-
-<meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content="https://bolaojpa.github.io/bolaoeuro2024/" />
-<meta property="twitter:title" content="Bolão Euro 2024" />
-<meta property="twitter:description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
-<meta property="twitter:image" content="https://i.ibb.co/FDkmQPS/Banner-Euro-2024-Azul-small.png" />
+<meta property="og:site_name" content="Bolão Euro 2024">
+    <meta property="og:title" content="">
+    <meta property="og:url" content="https://bolaojpa.github.io/bolaoeuro2024/" />
+    <meta property="og:description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO.">
+    <meta property="og:image:secure_url" itemprop="image" content="https://i.ibb.co/FDkmQPS/Banner-Euro-2024-Azul-small.png">
+    <meta property="og:type" content="website">
 
 
       
