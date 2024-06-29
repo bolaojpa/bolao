b0bc6646c73e1ce52c476f422cc6056d8d7181ab
Bolao JPA <bolaojpa@gmail.com>
2024-06-29 10:59:50 -0300

    Inserida metatags para redes sociais

--- assets/script/head.js
+++ assets/script/head.js
@@ -1,8 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var headContent = `
+document.addEventListener("DOMContentLoaded", function () {
+  var headContent = `
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Bolão Euro 2024</title>
+<title>Bolão Euro 2024</title>
+
+<meta name="title" content="Bolão Euro 2024" />
+<meta name="description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://bolaojpa.github.io/bolaoeuro2024/" />
+<meta property="og:title" content="Bolão Euro 2024" />
+<meta property="og:description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
+<meta property="og:image" content="https://i.ibb.co/FDkmQPS/Banner-Euro-2024-Azul-small.png" />
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://bolaojpa.github.io/bolaoeuro2024/" />
+<meta property="twitter:title" content="Bolão Euro 2024" />
+<meta property="twitter:description" content="Desde 2006 estamos realizando essa disputa saudável e competitiva entre amigos, sem fins lucrativos, sempre fazemos os bolões em épocas da EUROCOPA e COPA DO MUNDO." />
+<meta property="twitter:image" content="https://i.ibb.co/FDkmQPS/Banner-Euro-2024-Azul-small.png" />
+
+
+      
       <link rel="icon" type="image/x-icon" href="">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
@@ -14,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
       <link rel="stylesheet" href="./assets/css/popup.css" class="css">
       <script src="https://kit.fontawesome.com/bdcd1478e3.js" crossorigin="anonymous"></script>
     `;
-  
-    document.head.innerHTML += headContent;
-  });
-  
+
+  document.head.innerHTML += headContent;
+});
